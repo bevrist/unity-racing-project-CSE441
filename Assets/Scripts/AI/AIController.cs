6c3b841a16849b9e4b20bf32b1845113b9aa9cbf
ARod <rodriguezalex98@gmail.com>
2020-05-04 09:12:34 -0700

    Working on input methods

---
+++ Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIController : MonoBehaviour
+{
+    public GameObject[] checkpoints;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        gameObject.SendMessage("Rotate", 1.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        gameObject.SendMessage("Forward", 1.0f);
+    }
+}
--- Assets/Scripts/Karts/BallKart/BallKartMovement.cs
+++ Assets/Scripts/Karts/BallKart/BallKartMovement.cs
@@ -3,20 +3,40 @@ using System.Collections;
 
 public class BallKartMovement : MonoBehaviour
 {
+    private bool player = true;
+
     public Rigidbody rb;
     public float turnSpeed = 50;
     public float speed = 10;
 
+    private void Start()
+    {
+        if (gameObject.GetComponent<AIController>())
+            player = false;
+    }
+
     void Update()
     {
         float turnDirection = Input.GetAxis("Horizontal");
-        transform.position = rb.transform.position;
-        transform.Rotate(new Vector3(0, turnDirection * turnSpeed * Time.deltaTime, 0), Space.Self);
+        if (player)
+            Rotate(turnDirection);
     }
 
     void FixedUpdate()
     {
         float moveVertical = Input.GetAxis("Vertical");
-        rb.AddForce(transform.forward * speed * moveVertical);
+        if (player)
+            Forward(moveVertical);
+    }
+
+    void Rotate(float horizontal)
+    {
+        transform.position = rb.transform.position;
+        transform.Rotate(new Vector3(0, horizontal * turnSpeed * Time.deltaTime, 0), Space.Self);
+    }
+
+    void Forward(float vertical)
+    {
+        rb.AddForce(transform.forward * speed * vertical);
     }
 }
