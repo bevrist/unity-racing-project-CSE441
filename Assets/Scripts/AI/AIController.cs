d74e6e5aee701999b4cf7368fff7e39736c82d49
ARod <rodriguezalex98@gmail.com>
2020-05-08 13:19:43 -0700

    Working on AI

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -6,20 +6,25 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class AIController : MonoBehaviour
 {
+    private NavMeshAgent agent;
     public GameObject[] checkpoints;
 
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        gameObject.SendMessage("Rotate", 1.0f);
+        //gameObject.SendMessage("Rotate", 1.0f);
+        agent.SetDestination(checkpoints[0].transform.position);
+        NavMeshPath path = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, checkpoints[0].transform.position, NavMesh.AllAreas, path);
+        Debug.Log(path.corners[0]);
     }
 
     private void FixedUpdate()
     {
-        gameObject.SendMessage("Forward", 1.0f);
+        //gameObject.SendMessage("Forward", 1.0f);
     }
 }
