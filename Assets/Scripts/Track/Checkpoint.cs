b643d471ef21ddd2ae08319bee6a8f796fddac44
Brett Evrist <brettevrist10@gmail.com>
2020-05-12 15:18:07 -0700

    Completed basic checkpoint

---
+++ Assets/Scripts/Track/Checkpoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Checkpoint : MonoBehaviour
+{
+    public float checkpointWidth = 6;
+
+    GameObject leftCheckpointObject;
+    GameObject rightCheckpointObject;
+    GameObject triggerMesh;
+
+    void Start()
+    {
+        CalculateCheckpointPoisitions();
+    }
+
+    void Update()
+    {
+        if (!Application.isPlaying) //run this code only in editor
+        {
+            CalculateCheckpointPoisitions();
+        }
+    }
+
+    private void CalculateCheckpointPoisitions()
+    {
+        leftCheckpointObject = transform.GetChild(0).gameObject;
+        rightCheckpointObject = transform.GetChild(1).gameObject;
+        leftCheckpointObject.transform.position = transform.position + transform.right * -1 * (checkpointWidth / 2);
+        rightCheckpointObject.transform.position = transform.position + transform.right * (checkpointWidth / 2);
+#if UNITY_EDITOR
+        Debug.DrawLine(leftCheckpointObject.transform.position, (transform.up * 3f) + leftCheckpointObject.transform.position, Color.green);
+        Debug.DrawLine(rightCheckpointObject.transform.position, (transform.up * 3f) + rightCheckpointObject.transform.position, Color.blue);
+#endif
+        triggerMesh = transform.GetChild(2).gameObject;
+        triggerMesh.transform.localPosition = new Vector3(0,1.5f,0);
+        triggerMesh.transform.localScale = new Vector3(checkpointWidth, 3.0f, 0.2f);
+    }
+}
