81a0f20b8750018ae47bfabf7e9074e56a0c42f6
Brett Evrist <brettevrist10@gmail.com>
2020-05-16 01:40:53 -0700

    added checkpoint system to auto create global array of checkpoints

--- Assets/Scripts/Track/Checkpoint.cs
+++ Assets/Scripts/Track/Checkpoint.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class Checkpoint : MonoBehaviour
 {
+    public int checkpointNumber = -1;
     public float checkpointWidth = 6;
 
     GameObject leftCheckpointObject;
@@ -30,10 +31,8 @@ public class Checkpoint : MonoBehaviour
         rightCheckpointObject = transform.GetChild(1).gameObject;
         leftCheckpointObject.transform.position = transform.position + transform.right * -1 * (checkpointWidth / 2);
         rightCheckpointObject.transform.position = transform.position + transform.right * (checkpointWidth / 2);
-#if UNITY_EDITOR
         Debug.DrawLine(leftCheckpointObject.transform.position, (transform.up * 3f) + leftCheckpointObject.transform.position, Color.green);
         Debug.DrawLine(rightCheckpointObject.transform.position, (transform.up * 3f) + rightCheckpointObject.transform.position, Color.blue);
-#endif
         triggerMesh = transform.GetChild(2).gameObject;
         triggerMesh.transform.localPosition = new Vector3(0,1.5f,0);
         triggerMesh.transform.localScale = new Vector3(checkpointWidth, 3.0f, 0.2f);
