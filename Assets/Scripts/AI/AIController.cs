54e742fbd6077549ca8aafdf1ab89938be07c25f
ARod <rodriguezalex98@gmail.com>
2020-05-29 12:16:35 -0700

    Merged my changes to develop, issue with navmesh

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -8,20 +8,19 @@ public class AIController : VehicleController
     private NavMeshAgent agent;
     private int cPath = 0;
     private int cCheckpoint = 0;
-    private NavMeshPath path = new NavMeshPath();
+    private NavMeshPath path;
 
-<<<<<<< Updated upstream
-=======
+
     public float currentSpeed;
     public float maxSpeed = 5.0f;
     public float turnSpeed = 5.0f;
->>>>>>> Stashed changes
     public GameObject[] checkpoints;
 
     void Start()
     {
         agent = myVehicle.GetComponent<NavMeshAgent>();
         SetPath(myVehicle.transform.position, checkpoints[cCheckpoint].transform.position);
+        path = new NavMeshPath();
     }
 
     void Update()
@@ -40,20 +39,6 @@ public class AIController : VehicleController
             //SetPath(myVehicle.transform.position, checkpoints[cCheckpoint].transform.position);
         }
 
-<<<<<<< Updated upstream
-        /*Debug.Log("Location: " + agent.path.corners[0]);
-        Debug.Log(Quaternion.Lerp(myVehicle.transform.rotation, Quaternion.LookRotation(agent.path.corners[0] - myVehicle.transform.position), 5.0f * Time.deltaTime).eulerAngles);
-        if (Quaternion.Lerp(myVehicle.transform.rotation, Quaternion.LookRotation(agent.path.corners[0] - myVehicle.transform.position), 5.0f * Time.deltaTime).eulerAngles.y <= 160.0f)
-        {
-            Debug.Log("Turning Left");
-            SetTurnDirection(-10.0f);
-        } else if(Quaternion.Lerp(myVehicle.transform.rotation, Quaternion.LookRotation(agent.path.corners[0] - myVehicle.transform.position), 5.0f * Time.deltaTime).eulerAngles.y >= 100.0f)
-        {
-            Debug.Log("Turning Right");
-            SetTurnDirection(10.0f);
-        }*/
-        SetForwardSpeed(5.0f);
-=======
         if (Physics.Raycast(myVehicle.transform.position, Vector3.down, out RaycastHit hit, 2))
         {
             if (hit.collider.gameObject.tag == "Slow")
@@ -66,10 +51,12 @@ public class AIController : VehicleController
             }
         }
 
-        Quaternion rotation = Quaternion.Lerp(myVehicle.transform.rotation, Quaternion.LookRotation(agent.path.corners[1] - myVehicle.transform.position, myVehicle.transform.up), Time.deltaTime * turnSpeed);
-        myVehicle.transform.rotation = rotation;
-        SetForwardSpeed(currentSpeed);
->>>>>>> Stashed changes
+        if (agent.path.corners.Length > 0)
+        {
+            Quaternion rotation = Quaternion.Lerp(myVehicle.transform.rotation, Quaternion.LookRotation(agent.path.corners[1] - myVehicle.transform.position, myVehicle.transform.up), Time.deltaTime * turnSpeed);
+            myVehicle.transform.rotation = rotation;
+            SetForwardSpeed(currentSpeed);
+        }
     }
 
     void SetPath(Vector3 Current, Vector3 Destination)
